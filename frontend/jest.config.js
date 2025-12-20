d674764f038845aee5f91e38f63641d64bedd025
islammokdadi-arch <islammokdadi1@gmail.com>
2025-12-20 15:23:30 +0100

    feat: Add test suite, CI/CD pipeline, and SonarQube integration - Added Django test suite (models, views, serializers) - Added pytest configuration - Added GitHub Actions workflow for SonarQube - Added SonarQube configuration - Added React test setup (Jest, Testing Library) - Configured test coverage reporting

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,17 +1,19 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    moduleNameMapper: {
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-      '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
-    },
-    testPathIgnorePatterns: ['/node_modules/', '/build/'],
-    collectCoverage: true,
-    collectCoverageFrom: [
-      'src/**/*.{js,jsx}',
-      '!src/**/*.test.{js,jsx}',
-      '!src/index.js',
-      '!src/reportWebVitals.js',
-    ],
-    coverageReporters: ['text', 'lcov', 'html'],
-  };
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/**/*.test.{js,jsx}',
+    '!src/main.jsx',
+  ],
+  coverageReporters: ['text', 'lcov', 'html'],
+};
